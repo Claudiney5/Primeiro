3d075ec82073af426de556c18b7858225d3e6b7d
Claudiney <claudiney.mail@gmail.com>
2020-01-11 01:31:50 -0200

    Tabelas da criação dos personagens.

---
+++ geracao.py
@@ -0,0 +1,14 @@
+"""
+GERAÇÃO
+
+Este módulo faz os cálculos randômicos e cria um GUI.
+
+"""
+from tkinter import *
+
+# passo 1: Escolher raça e profissão -> Usuário (todos as demais escolhas serão randômicas)
+
+
+
+root = Tk()
+mainloop()
